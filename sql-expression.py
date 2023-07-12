30ed7bcc8623603e0bd639014c4df93adfb730f5
thenriq <>
2023-07-12 17:08:23 +0000

    add ORM

--- sql-expression.py
+++ sql-expression.py
@@ -55,7 +55,8 @@ with db.connect() as connection:
     # Query 5 - select only the albuns with "ArtistId" #51 on the "Album" table
     # select_query = album_table.select().where(album_table.c.ArtistId == 51)
 
-    # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+    # Query 6 - select all tracks
+    # where the composer is "Queen" from the "Track" table
 
     results = connection.execute(select_query)
     for result in results:
--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -22,7 +22,8 @@ cursor = connection.cursor()
 # Query 5 - select only the albuns with "ArtistId" #51 on the "Album" table
 cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# Query 6 - select all tracks
+# where the composer is "Queen" from the "Track" table
 cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
 # fetch the results (multiple)
